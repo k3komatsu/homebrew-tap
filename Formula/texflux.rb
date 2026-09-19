88d216d2fe8b636443f2fa1c7286a2c2a2a5e6d9
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2026-09-19 22:46:39 +0900

    feat: add texflux formula

---
+++ Formula/texflux.rb
@@ -0,0 +1,19 @@
+class Texflux < Formula
+  desc "TeX-first, indentation-based preprocessor for LaTeX and Beamer"
+  homepage "https://github.com/k3komatsu/TeXFlux"
+  url "https://github.com/k3komatsu/TeXFlux/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "91189ee68197d99301ebdf7705258eab6a62e753e3ebfe198a04331983f59dcc"
+  license "0BSD"
+
+  depends_on "dub" => :build
+  depends_on "ldc" => :build
+
+  def install
+    system "dub", "build", "--build=release"
+    bin.install "bin/texflux"
+  end
+
+  test do
+    assert_match "texflux 0.3.0", shell_output("#{bin}/texflux --version")
+  end
+end
