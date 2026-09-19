6367dde6ed9681a570cffec1d553e72d729ffa26
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2026-09-19 23:41:37 +0900

    texflux 0.3.1
    Closes #2.
    
    Signed-off-by: Kazuki Komatsu <1687398+k3komatsu@users.noreply.github.com>

--- Formula/texflux.rb
+++ Formula/texflux.rb
@@ -1,8 +1,8 @@
 class Texflux < Formula
   desc "TeX-first, indentation-based preprocessor for LaTeX and Beamer"
   homepage "https://github.com/k3komatsu/TeXFlux"
-  url "https://github.com/k3komatsu/TeXFlux/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "91189ee68197d99301ebdf7705258eab6a62e753e3ebfe198a04331983f59dcc"
+  url "https://github.com/k3komatsu/TeXFlux/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "c7329a1d37aaa11c5a6dfee455d13bea8b30d3ecac97a1ea668bae9676813f5b"
   license "0BSD"
 
   depends_on "dub" => :build
