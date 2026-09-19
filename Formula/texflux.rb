8a09ae58a01bbd34dbd65a4f2dab7a80ba433aed
Kazuki Komatsu <1687398+k3komatsu@users.noreply.github.com>
2026-09-19 14:46:51 +0000

    texflux: add 0.3.1 bottle.

--- Formula/texflux.rb
+++ Formula/texflux.rb
@@ -5,6 +5,12 @@ class Texflux < Formula
   sha256 "c7329a1d37aaa11c5a6dfee455d13bea8b30d3ecac97a1ea668bae9676813f5b"
   license "0BSD"
 
+  bottle do
+    root_url "https://ghcr.io/v2/k3komatsu/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "700ae56c32c888cc5ae95b52c6c144fdeb0d660721a5b3bef3dba71fc5c80a6c"
+    sha256 cellar: :any,                 x86_64_linux: "8b6081df9536b97ecb6d63d96a2add84ecc085159310d94d8be535eb4fbeafed"
+  end
+
   depends_on "dub" => :build
   depends_on "ldc" => :build
 
